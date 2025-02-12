c271db44835f87dab1037b4dde518582bd37150c
kostjakotovich <78017690+kostjakotovich@users.noreply.github.com>
2025-02-12 09:41:29 +0200

    Update LL_1.py

--- LL_1.py
+++ LL_1.py
@@ -29,13 +29,20 @@ class LinkedList:
     #                                    #
     ######################################
 
-
+    def find_middle_node(self):
+        slow=self.head
+        fast=self.head
+        while fast is not None and fast.next is not None:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
 
 my_linked_list = LinkedList(1)
 my_linked_list.append(2)
 my_linked_list.append(3)
 my_linked_list.append(4)
 my_linked_list.append(5)
+my_linked_list.append(6)
 
 print( my_linked_list.find_middle_node().value )
 
